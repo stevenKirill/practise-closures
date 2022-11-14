4c717d89fa4eaf6cdfea601cd8921b1a2dbe78b9
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-11-14 20:24:40 +0300

    started tom code calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,35 @@
 function calculator() {
-    return function (str) {
-        return undefined
+    var state = {
+        result: 0,
+        current: '',
+    }
+    const onlyNumbers = new Regex("^\\d+$");
+    const signs = /+|-|*|\//gi;
+    const operations = {
+        '+': function(num1, num2) {
+            return Number(num1) + Number(num2);
+        },
+        '-': function(num1, num2) {
+            return Number(num1) - Number(num2);
+        },
+        '*': function(num1, num2) {
+            return Number(num1) * Number(num2);
+        },
+        '/': function(num1, num2) {
+            return Number(num1) / Number(num2);
+        }
+    };
+    return function (symbol) {
+        if (onlyNumbers.test(symbol)) {
+            state.current += symbol;
+        }
+        if (signs.test(symbol)) {
+            operations[symbol]()
+        }
+        if (symbol === '=') {
+            const { result } = state;
+            console.log(result)
+        }
     };
 }
 
@@ -9,6 +38,7 @@ var calc = calculator();
 function useCalc(calc, keys) {
     return [...keys].reduce(function showDisplay(display, key) {
         var ret = String(calc(key));
+        console.log(ret, '=> ret');
         return (
             display + (
                 (ret !== '' && key === '=') ? '=' : ''
@@ -17,4 +47,40 @@ function useCalc(calc, keys) {
     }, '')
 }
 
-useCalc(calc, '4+3=')
+useCalc(calc, '4+3=') // 4+3=7
+// useCalc(calc, '+9=') // 7+9=16
+// useCalc(calc, '*8=') // 16*8=128
+// useCalc(calc, '7*2*3') // 7*2*3=42
+// useCalc(calc, '1/0=') // 1/0=ERR
+// useCalc(calc, '+3') // +3=ERR
+// useCalc(calc, '51=') // 51
+
+function formatTotal(display) {
+    if (Number.isFinite(display)) {
+        let maxDigits = 11;
+        if (Math.abs(display) > 99999999999) {
+            maxDigits -= 6;
+        }
+
+        if (display < 0) {
+            maxDigits--;
+        }
+
+        if (Number.isInteger(display)) {
+            display = display
+            .toPrecision(maxDigits)
+            .replace(/\.0+$/,'')
+        } else {
+            maxDigits--;
+            if (Math.abs(display) >= 0 && Math.abs(display) < 1) {
+                maxDigits--;
+            }
+            display = display
+            .toPrecision(maxDigits)
+            .replace(/0+$/,'')
+        }
+    } else {
+        display = 'ERR'
+    }
+    return display;
+}
