46f689722a5fd53bb3551f93c48bf188fcb26818
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-11-18 18:30:05 +0300

    added logs

--- calculator.js
+++ calculator.js
@@ -1,16 +1,13 @@
 // мой вариант
 function calculator() {
-    var state = {
-        result: 0,
-        current: '',
-        currentSing: 0,
-    }
-
-    console.log(state, '=> state');
+    var result = 0;
+    var currentDigit =  ''; // 4
+    var currentSing = '';
     const onlyNumbers = /\d/;
     const signs = /[+/*-]/;
-    const operations = {
+    var operations = {
         '+': function(num1, num2) {
+            console.log(Number(num1), '=> два вот этих числа')
             return Number(num1) + Number(num2);
         },
         '-': function(num1, num2) {
@@ -23,74 +20,47 @@ function calculator() {
             return Number(num1) / Number(num2);
         }
     };
-    return function (symbol) {
+    function pressCalculatorBatton(symbol) {
+        console.log(currentDigit, '=> currentDigit')
+        console.log(currentSing, '=> currentSing')
         if (onlyNumbers.test(symbol)) {
-            state.current = symbol;
-        }
-        if (signs.test(symbol)) {
-            state.currentSing = symbol;
-        }
-        if (symbol === '=') {
-            let { result, currentSing, current } = state;
-            result = operations[currentSing](current, Number(symbol))
-            return result;
+            console.log(symbol, '=> цифры')
+            currentDigit += symbol;
+            return;
+        } else if (signs.test(symbol)) {
+            console.log(symbol, '=> знак')
+            // кейс серии из нескольких операций
+            if (currentSing !== '=' && currentDigit !== '') {
+                pressCalculatorBatton('=');
+            } else if (currentDigit !== '') {
+                result = Number(symbol);
+                return 
+            }
+            currentSing = symbol;
+            currentDigit = '';
+            return;
+        } else if (symbol === '=' && currentSing !== '') {
+            result = operations[currentSing](currentDigit, symbol)
+            currentSing = '=';
+            currentDigit = '';
+            return formatTotal(result);
         }
     };
+    return pressCalculatorBatton;
 }
 
-function useCalc(calc, keys) {
-    return [...keys].reduce(function showDisplay(display, key) {
-        var ret = String(calc(key));
-        return (
-            display + (
-                (ret !== '' && key === '=') ? '=' : ''
-            ) + ret
-        )
-    }, '')
-}
-
-var calc = calculator2();
+var calc = calculator();
 
 var first = useCalc(calc, '4+3=') // 4+3=7
-console.log(first)
-var second = useCalc(calc, '+9=') // 7+9=16
-console.log(second)
-var third = useCalc(calc, '*8=') // 16*8=128
-console.log(third)
+console.log(first, '=> первый результат')
+// var second = useCalc(calc, '+9=') // 7+9=16
+// console.log(second)
+// var third = useCalc(calc, '*8=') // 16*8=128
+// console.log(third)
 // useCalc(calc, '7*2*3') // 7*2*3=42
 // useCalc(calc, '1/0=') // 1/0=ERR
 // useCalc(calc, '+3') // +3=ERR
 // useCalc(calc, '51=') // 51
-
-function formatTotal(display) {
-    if (Number.isFinite(display)) {
-        let maxDigits = 11;
-        if (Math.abs(display) > 99999999999) {
-            maxDigits -= 6;
-        }
-
-        if (display < 0) {
-            maxDigits--;
-        }
-
-        if (Number.isInteger(display)) {
-            display = display
-            .toPrecision(maxDigits)
-            .replace(/\.0+$/,'')
-        } else {
-            maxDigits--;
-            if (Math.abs(display) >= 0 && Math.abs(display) < 1) {
-                maxDigits--;
-            }
-            display = display
-            .toPrecision(maxDigits)
-            .replace(/0+$/,'')
-        }
-    } else {
-        display = 'ERR'
-    }
-    return display;
-}
 
 // пример из учебника
 function calculator2() {
@@ -132,4 +102,47 @@ function calculator2() {
         }
         return ops[oper](val1, val2);
     }
+}
+
+/** использование калькулятора */
+function useCalc(calc, keys) {
+    return [...keys].reduce(function showDisplay(display, key) {
+        var ret = String(calc(key));
+        return (
+            display + (
+                (ret !== '' && key === '=') ? '=' : ''
+            ) + ret
+        )
+    }, '')
+}
+
+/** форматирование */
+function formatTotal(display) {
+    if (Number.isFinite(display)) {
+        let maxDigits = 11;
+        if (Math.abs(display) > 99999999999) {
+            maxDigits -= 6;
+        }
+
+        if (display < 0) {
+            maxDigits--;
+        }
+
+        if (Number.isInteger(display)) {
+            display = display
+            .toPrecision(maxDigits)
+            .replace(/\.0+$/,'')
+        } else {
+            maxDigits--;
+            if (Math.abs(display) >= 0 && Math.abs(display) < 1) {
+                maxDigits--;
+            }
+            display = display
+            .toPrecision(maxDigits)
+            .replace(/0+$/,'')
+        }
+    } else {
+        display = 'ERR'
+    }
+    return display;
 }
